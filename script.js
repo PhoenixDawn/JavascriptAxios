c0cff8bba2a54719e5740a4e839a290f3d6967c1
Dane Thomson <dane.thomson@outlook.com>
2021-06-15 20:58:25 +1000

    Added favourites

--- script.js
+++ script.js
@@ -47,6 +47,10 @@ function removeOldList() {
     let ship = document.querySelector(`.shipDisplay:nth-child(1)`);
     ship.remove();
   }
+  let infoDiv = document.querySelector("#destroyMe");
+  if (infoDiv) {
+    infoDiv.remove();
+  }
 }
 
 // Display Information
@@ -59,7 +63,8 @@ function displayData(info) {
     let pHomePort = document.createElement("p");
     let pType = document.createElement("p");
     let pWeight = document.createElement("p");
-    let btn = document.createElement("button");
+    let btnBack = document.createElement("button");
+    let btnFav = document.createElement("button");
 
     let divRole = document.createElement("div");
     let pRoles = document.createElement("p");
@@ -79,7 +84,8 @@ function displayData(info) {
     pType.innerHTML = `Type: ${item.type}`;
     pRoles.innerHTML = "Roles:";
     pWeight.innerHTML = `Weight kg: ${item.mass_kg}`;
-    btn.innerHTML = "Back";
+    btnBack.innerHTML = "Back";
+    btnFav.innerHTML = "Add to favourite";
     image.src = item.image;
 
     h3.innerHTML = item.name;
@@ -91,17 +97,41 @@ function displayData(info) {
     div.appendChild(pRoles);
     div.appendChild(divRole);
     div.appendChild(image);
-    div.append(btn);
+    div.append(btnBack);
+    div.append(btnFav);
+    div.id = "destroyMe";
     displayDiv.append(div);
 
     //Add a back button
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log("TEST");
+    btnBack.addEventListener("click", (e) => {
       div.remove();
-      getShipNames()
+      getShipNames();
+    });
+    btnFav.addEventListener("click", (e) => {
+      addToFavorite(item.name);
     });
   }
+}
+
+function addToFavorite(name) {
+  let favDiv = document.querySelector(".favorites");
+  let li = document.createElement("li");
+  let btn = document.createElement("button");
+  li.innerHTML = `${name}`; // <button id='removeBtn'>REMOVE</button>`
+  btn.innerHTML = `Remove`; // <button id='removeBtn'>REMOVE</button>`
+  favDiv.append(li);
+  favDiv.append(btn);
+
+  li.addEventListener("click", (e) => {
+    e.stopPropagation();
+    makeRequest(name);
+  });
+
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    btn.remove();
+    li.remove();
+  });
 }
 
 // Search Button Click
